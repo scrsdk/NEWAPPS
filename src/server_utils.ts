da78da732c55c62ae1dce37ad34535ed3fb2a7a2
KirScr <142673005+scrsdk@users.noreply.github.com>
2025-07-17 20:32:58 +0300

    Update server_utils.ts

--- src/server_utils.ts
+++ src/server_utils.ts
@@ -1,5 +1,9 @@
 'use server'
 
+import axios from "axios"
+import { UserModel } from "./@types/data.t"
+import { cookies } from "next/headers"
+
 const isLoginCheck = async (): Promise<UserModel | null> => {
         const url = `${process.env.NEXT_PUBLIC_BASE_PATH}/api/auth/me`;
         console.log("Attempting to connect to:", url); // Добавьте это
