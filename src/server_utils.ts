a4dcabdca4936962c481fbf26d0d69c03411e238
KirScr <142673005+scrsdk@users.noreply.github.com>
2025-07-17 20:32:24 +0300

    Update server_utils.ts

--- src/server_utils.ts
+++ src/server_utils.ts
@@ -1,14 +1,13 @@
 'use server'
 
-import axios from "axios"
-import { UserModel } from "./@types/data.t"
-import { cookies } from "next/headers"
-
 const isLoginCheck = async (): Promise<UserModel | null> => {
-    const userData = await axios.post(`${process.env.NEXT_PUBLIC_BASE_PATH}/api/auth/me`, cookies().get('token')?.value)
-    return userData.data ?? null
-}
-
-export {
-    isLoginCheck,
-}
+        const url = `${process.env.NEXT_PUBLIC_BASE_PATH}/api/auth/me`;
+        console.log("Attempting to connect to:", url); // Добавьте это
+        try {
+            const userData = await axios.post(url, cookies().get('token')?.value);
+            return userData.data ?? null;
+        } catch (error) {
+            console.error("Error connecting to backend:", error.message); // Добавьте это
+            return null;
+        }
+    }
